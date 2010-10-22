028b1599d68b3d1c3c3246acd8e7308a0c69b2d6
cvpcs <root@cvpcs.org>
2010-10-21 22:02:12 -0500

    fixed the memory issue with factor stuffs and added some comments explaining what i did

--- Factors.java
+++ Factors.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -8,23 +9,38 @@ public class Factors{
 		factorsWork(n);
 	}
 	public static List<Long> factorsWork(long n){
-		List<Long> list1 = new ArrayList<Long>(Arrays.asList(new Long[]{}));
-		List<Long> list2 = new ArrayList<Long>(Arrays.asList(new Long[]{}));
-		List<Long> list3 = new ArrayList<Long>(Arrays.asList(new Long[]{}));
+		// you don't need to add all that array crap when creating array lists, all it
+		// does is allocate memory that array list will overwrite anyway
+		List<Long> list1 = new ArrayList<Long>();
+		List<Long> list2 = new ArrayList<Long>();
+		List<Long> list3 = new ArrayList<Long>();
 		for (long i = 2;i <= Math.sqrt(n+1);i++){
 			if(n%i==0){
-				list1.add(n);
-			} for (int j = 0; j < list1.size(); j++){
-				long c = n / list1.get(j);
-				for (int k = 0; k < list1.size(); k++){
-					if(list1.get(k)!= c){
-						list2.add(c);
-					}
-				}	
+				// need to add the iterator to the list, not the test number, don't
+				// feel bad, i spent like... a solid 30 minutes debugging this code
+				// before i noticed this collossal annoyance >< lol
+				list1.add(i);
 			}
 		}
+		// i moved the part where you deal with list two outside of the other loop, to avoid
+		// errors related to nested loops and the confusion they can cause.
+		// we can wait to do this until the end, since we are essentially
+		// just adding the factors > sqrt(n) that correspond to those < sqrt(n)
+		for (int j = 0; j < list1.size(); j++){
+			long c = n / list1.get(j);
+			// only add to second list if it doesn't already exist in either
+			// of the two lists (since they'll be concatinated)
+			if( !(list1.contains(c) || list2.contains(c)) ){
+				list2.add(c);
+			}
+		}
+
 		list3.addAll(list1);
 		list3.addAll(list2);
+
+		// added a simple ascending sort, as per the handy-dandy pseudo code provided below
+		Collections.sort(list3);
+
 		return list3;
 	}
 }
